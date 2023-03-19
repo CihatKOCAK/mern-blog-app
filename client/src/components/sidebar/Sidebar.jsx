ec1ea4c278b66f181d0c69de2662cdf2baffa49c
Cihat KOCAK <pcihatkocakks@gmail.com>
2023-03-19 19:22:25 +0300

    SideBar - Single Post Comp and Page OK

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,39 @@
-import "./sidebar.css"
+import "./sidebar.css";
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">Sidebar</div>
-  )
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt=""
+        />
+        <p>
+          Ad nulla duis enim officia.Anim ullamco qui eu reprehenderit nisi sit
+          ipsum sunt officia minim quis consectetur.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
